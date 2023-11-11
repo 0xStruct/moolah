c96753cca3eaf3bf5358147ac5cf998e61b7c850
0xstruct <0xstruct@gmail.com>
2023-11-11 07:02:52 +0630

    oracle tweaks
    oracle tweaks

--- oracle/mina-email-oracle.ts
+++ oracle/mina-email-oracle.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import { Request, Response } from 'express';
 import { dkimVerify } from 'mailauth/lib/dkim/verify';
 import { parseHeaders } from 'mailauth/lib/tools';
-import { PrivateKey, Field, Signature } from 'o1js';
+import { PrivateKey, Field, Signature, CircuitString, UInt32, Poseidon, Sign } from 'o1js';
 import fs from 'fs';
 
 const dotenv = require('dotenv');
@@ -25,7 +25,6 @@ app.get('/test', async (req: Request, res: Response) => {
 	// load .eml files
 	let emlPass = await fs.promises.readFile('test-pass.eml', { encoding: 'utf-8' });
 	let emlFake = await fs.promises.readFile('test-fake.eml', { encoding: 'utf-8' }); // changed mina amount
-
 
 	const resultPass = await dkimVerify(emlPass);
 
@@ -65,6 +64,57 @@ app.get('/test', async (req: Request, res: Response) => {
 
 	res.send({ 
 		message: `Mina Email Oracle - testing, check console log of the server`,
+	});
+});
+
+app.get('/sign', async (req: Request, res: Response) => {
+	// load .eml files
+	let emlPass = await fs.promises.readFile('test-pass.eml', { encoding: 'utf-8' });
+
+	const resultPass = await dkimVerify(emlPass);
+
+	// get email metadata such as to, from, subject
+	const headers = parseHeaders(emlPass);
+
+	var from, messageId, subject;
+	const from_regex = /\<(.*?)\>/gi;
+	const messageId_regex = /\<(.*?)\@/gi;
+
+	for (let h of headers.parsed) {
+		if(h.key === 'from') from = from_regex.exec(h.line.toString())[1];
+		if(h.key === 'message-id') messageId = messageId_regex.exec(h.line.toString())[1];
+		if(h.key === 'subject') subject = h.line.toString().split(': ')[1];
+		//if(h.key === 'dkim-signature') console.log(h.line.toString());
+	}
+
+	console.log('Email metadata: ', from, messageId, subject);
+	//console.log(resultPass)
+	//console.log(emlPass)
+
+	// loop through results
+	for (let { info } of resultPass.results) {
+		console.log('### test-pass.eml')
+		console.log(info);
+		console.log(info.startsWith('dkim=pass')); //pass, temperror, fail
+	}
+
+	// Oracle Public and Private Key
+	const privateKey = PrivateKey.fromBase58(process.env.PRIVATE_KEY);
+	const publicKey = privateKey.toPublicKey();
+	
+	const amount = 1;
+
+	const emailHashCircuit = CircuitString.fromString(messageId);
+	const signedDataFields = emailHashCircuit.toFields().concat(UInt32.from(amount).toFields());
+	console.log('Poseidon hash: ', Poseidon.hash(signedDataFields).toString());
+
+	//const signature = Signature.create(privateKey, [Poseidon.hash(signedDataFields)]);
+	const signature = Signature.create(privateKey, [ Field(amount) ]); // need to figure out a way to pass Poseidon hash
+
+	res.send({
+		data: { emailHash: messageId, amount: amount },
+		signature: signature,
+		publicKey: publicKey,
 	});
 });
 
@@ -109,16 +159,16 @@ app.post('/', async (req: Request, res: Response) => {
 
 		console.log('Email metadata: ', from, messageId, subject);
 
-		const emailId = '';
+		const emailHash = '';
 		const amount = '';
 
 		const signature = Signature.create(privateKey, [
-			Field(emailId),
+			Field(emailHash),
 			Field(amount),
 		]);
 
 		res.send({
-			data: { emailId, amount },
+			data: { emailHash, amount },
 			signature: signature,
 			publicKey: publicKey,
 		});
